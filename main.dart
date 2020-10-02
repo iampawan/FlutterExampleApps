18be2faab6f5f70b49f23611a27391593a4d7b23
raza4399 <52241717+raza4399@users.noreply.github.com>
2020-10-02 08:19:07 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,21 @@
-void main() {
-  print("Flutter is amazing");
-  print(
-      "Flutter Example apps repo is the collections of awesome apps built with flutter");
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        Text(
+          "updated app",
+           style: TextStyle(
+           color: Colors.red,
+           fontSize: 18,
+           fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
+
