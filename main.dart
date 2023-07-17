c95f41a62bbc8312461f6d4d9accd2d6821726d7
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2023-07-17 18:25:14 +0530

    Added new variables

--- main.dart
+++ main.dart
@@ -2,4 +2,12 @@ void main() {
   print("Flutter is amazing");
   print(
       "Flutter Example apps repo is the collections of awesome apps built with flutter");
+
+  String name = 'John';
+  int age = 25;
+  double height = 1.75;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
 }
